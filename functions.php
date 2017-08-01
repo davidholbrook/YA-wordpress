f776222a7ac0b545a1d2cfe10b276b4f047c4dd6
David Holbrook <david@davidholbrook.info>
2017-08-01 00:27:29 -0500

    deleted some unused files
    Signed-off-by: David Holbrook <david@davidholbrook.info>

--- functions.php
+++ functions.php
@@ -25,7 +25,6 @@ if ( ! function_exists( 'yeah_academy_setup' ) ) :
 		load_theme_textdomain( 'yeah-academy', get_template_directory() . '/languages' );
 
 		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
 
 		/*
 		 * Let WordPress manage the document title.
@@ -60,25 +59,14 @@ if ( ! function_exists( 'yeah_academy_setup' ) ) :
 		) );
 
 		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'yeah_academy_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
-		) ) );
 
 		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
 
 		/**
 		 * Add support for core custom logo.
 		 *
 		 * @link https://codex.wordpress.org/Theme_Logo
 		 */
-		add_theme_support( 'custom-logo', array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		) );
 	}
 endif;
 add_action( 'after_setup_theme', 'yeah_academy_setup' );
@@ -90,28 +78,12 @@ add_action( 'after_setup_theme', 'yeah_academy_setup' );
  *
  * @global int $content_width
  */
-function yeah_academy_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'yeah_academy_content_width', 640 );
-}
-add_action( 'after_setup_theme', 'yeah_academy_content_width', 0 );
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function yeah_academy_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'yeah-academy' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'yeah-academy' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'yeah_academy_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
@@ -147,7 +119,6 @@ require get_template_directory() . '/inc/template-functions.php';
 /**
  * Customizer additions.
  */
-require get_template_directory() . '/inc/customizer.php';
 
 /**
  * Load Jetpack compatibility file.
@@ -155,3 +126,11 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
+
+define('JETPACK_DEV_DEBUG', true);
+
+
+function my_custom_login() {
+echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('stylesheet_directory') . '/assets/custom-login-styles.css" />';
+}
+add_action('login_head', 'my_custom_login');
